43b1b3fae119a31c168f03f5fe52271bad46e3f7
Karine <karine.ohanyan95@gmail.com>
2021-10-20 18:26:12 +0400

    Add project files.

---
+++ CDemo/Migrations/20211020142033_AddedUserPersonalDate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CDemo.Migrations
+{
+    public partial class AddedUserPersonalDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
